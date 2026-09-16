d0e4b9e5b587019eee5c7a2f305aec9e8bf81406
Mug-Maniac <andrewzinzi@gmail.com>
2026-09-16 19:28:57 -0400

    Implemented add_product method. Pass a product through, if it isnt already in products list, append to list return true, if already in list return false

--- store.py
+++ store.py
@@ -1,9 +1,14 @@
 
 
 class Store:
-    #constructor
+
     def __init__(self):
         self.products = []
         self.customers = []
     
-    
+    def add_product(self, product: Product):
+        if product.id in self.products:
+            return False
+        else:
+            self.products.append(product.id)
+            return True
