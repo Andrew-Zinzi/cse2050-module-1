c11ae45bbf9848dde00b1bded99f1be731e2fd8a
Mug-Maniac <andrewzinzi@gmail.com>
2026-09-16 20:02:39 -0400

    fixed logic, added correct methods from customers and products classes, shortened docstrings

--- store.py
+++ store.py
@@ -1,66 +1,40 @@
-
-
 class Store:
+    """Represents the marketplace managing products and customers"""
 
     def __init__(self):
         """constructor for Store class"""
         self.products = []
         self.customers = []
     
-    def add_product(self, product: Product):
-        """Add product to products list if not already in list
-
-        Args:
-            product (Product): instance of product class
-
-        Returns:
-            bool: was product added to list, returns true if successful or false if not successful
-        """
-        if product.id in self.products:
+    def add_product(self, product) -> bool:
+        """Initializes empty product and customer lists"""
+        if self.find_product(product.get_id()) is not None:
             return False
-        else:
-            self.products.append(product.id)
-            return True
-    
-    def find_product(self, product: Product):
-        """Find product in products list
-
-        Args:
-            product (Product): instance of product class
-
-        Returns:
-            bool, Product: returns false if not in list, returns instance of product if in list 
-        """
-        if product.id in self.products:
-            return product
-        else:
-            return None
-    
-    def add_customer(self, customer: Customer):
-        """Add customer to customer list
-
-        Args:
-            customer (Customer): instance of customer class 
-
-        Returns:
-            bool: was customer added to list, returns true if successful or false if not successful
-        """
-        if customer.id in self.customers:                
-            return False
-        else:
-            self.customers.append(customer.id)
-            return True
         
-    def find_customer(self, customer: Customer):
-        """Find customer in customers list 
-
-        Args:
-            customer (Customer): instance of customer class
-
-        Returns:
-            bool, Customer: returns false if not in list, returns instance of customer if in list
-        """
-        if customer.id in self.customers:
-            return customer
-        else:
-            return None
+        self.products.append(product)
+        return True
+    
+    def find_product(self, product_id: str):
+        """Find and return a Product by ID; return None when not found"""
+        for item in self.products:
+            if item.get_id() == self.products_id:
+                return item
+            
+        return None
+    
+    def add_customer(self, customer) -> bool:
+        """Add a customer if its ID is not already stored"""
+        if self.find_customer(customer.get_id()) is not None:
+            return False
+        
+        self.customers.append(customer)
+        return True
+            
+        
+    def find_customer(self, customer_id: str):
+        """Find and return a Customer by ID; return None when not found"""
+        for item in self.customers:
+            if item.get_id() == customer_id:
+                return item
+        
+        return None
