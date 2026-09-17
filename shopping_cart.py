10a4d63d186c066435820f66812e1acfccf67316
Mug-Maniac <andrewzinzi@gmail.com>
2026-09-16 20:07:48 -0400

    created shopping cart class, outlined methods and built constructor

---
+++ shopping_cart.py
@@ -0,0 +1,17 @@
+class ShoppingCart:
+    """Represents cart customers put products into"""
+    def __init__(self):
+        """constructor for ShoppingCart class"""
+        self.items = []
+        
+    def add_product():
+        
+    
+    def remove_product():
+        
+    def get_items():
+        
+    def calculate_total():
+        
+    def is_empty():
+    
