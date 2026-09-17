89f5bb7343b0e02b2c26042621d558278b849bde
Mug-Maniac <andrewzinzi@gmail.com>
2026-09-16 20:17:28 -0400

    added docstrings to all methods

--- shopping_cart.py
+++ shopping_cart.py
@@ -5,17 +5,21 @@ class ShoppingCart:
         self.items = []
         
     def add_product(self, product):
+        """Add product to items list"""
         self.items.append(product)
         
     def remove_product(self, product_id: str):
+        """Find item in list remove if exists return true/false if successful"""
         for item in self.items:
             if item.get_id == product_id:
                 self.items.remove(item)
         
     def get_items(self):
+        """Return list of all items in cart"""
         return self.items
         
     def calculate_total(self) -> float:
+        """returns total price of all items in cart"""
         total = 0
         for item in self.items:
             total += item.get_price()
@@ -23,6 +27,7 @@ class ShoppingCart:
         return total
         
     def is_empty(self):
+        """return wether or not cart is empty"""
         if len(self.items) == 0:
             return True
         else:
