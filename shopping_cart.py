9bf202f406533bac8c1bb5a949cd631517bd9350
Mug-Maniac <andrewzinzi@gmail.com>
2026-09-16 20:14:20 -0400

    Implemented method behaviors

--- shopping_cart.py
+++ shopping_cart.py
@@ -4,14 +4,26 @@ class ShoppingCart:
         """constructor for ShoppingCart class"""
         self.items = []
         
-    def add_product():
+    def add_product(self, product):
+        self.items.append(product)
         
-    
-    def remove_product():
+    def remove_product(self, product_id: str):
+        for item in self.items:
+            if item.get_id == product_id:
+                self.items.remove(item)
         
-    def get_items():
+    def get_items(self):
+        return self.items
         
-    def calculate_total():
+    def calculate_total(self) -> float:
+        total = 0
+        for item in self.items:
+            total += item.get_price()
+            
+        return total
         
-    def is_empty():
-    
+    def is_empty(self):
+        if len(self.items) == 0:
+            return True
+        else:
+            return False
